2d810e50ffa7e3318047c21c5d47af6e55fdaa90
Noah Flaniken <nhflan@comcast.net>
2020-01-31 16:06:52 -0500

    add functions to navcontainer for each button

--- VSSquad/NavContainer/NavContainerViewController.swift
+++ VSSquad/NavContainer/NavContainerViewController.swift
@@ -11,12 +11,22 @@ import UIKit
 class NavContainerViewController: UIViewController {
 
     @IBOutlet weak var QRView: UIView!
+    @IBOutlet weak var homeView: UIView!
+    @IBOutlet weak var myEventsView: UIView!
+    @IBOutlet weak var addEventsView: UIView!
+    @IBOutlet weak var searchEventsView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        QRView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(scanQR)))
+        homeView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(goHome)))
+        myEventsView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(viewMyEvents)))
+        addEventsView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(addEvent)))
+        searchEventsView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(searchEvents)))
+        
     }
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -26,14 +36,35 @@ class NavContainerViewController: UIViewController {
         
     }
 
-    /*
+    // MARK: Actions
+    
+    @objc func scanQR (){
+        print("scanQR")
+    }
+    
+    @objc func goHome(){
+        print("goHome")
+    }
+    
+    @objc func viewMyEvents(){
+        print("viewMyEvents")
+    }
+    
+    @objc func addEvent(){
+        print("addEvent")
+    }
+    
+    @objc func searchEvents(){
+        print("searchEvents")
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        
     }
-    */
+    
 
 }
